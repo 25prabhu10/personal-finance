9c7542ea11be699e75876862b1480f633b16bd24
Prabhu K H <25prabhu10@gmail.com>
2025-04-10 22:24:32 -0400

    chore: add simple-git-hooks and lint-staged

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -5,17 +5,18 @@ import nodePlugin from 'eslint-plugin-n'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(nodePlugin.configs['flat/recommended-script'], {
-  extends: [...baseConfig],
+export default tseslint.config(nodePlugin.configs['flat/recommended-script'], baseConfig, {
+  files: ['**/*.{js,jsx,ts,tsx}'],
   languageOptions: {
     ecmaVersion: 'latest',
     globals: globals.node,
     parserOptions: {
       projectService: {
-        allowDefaultProject: ['*.js'],
+        allowDefaultProject: ['*.js', '.prettierrc.js'],
         defaultProject: 'tsconfig.json'
       },
       tsconfigRootDir: import.meta.dirname
-    }
+    },
+    sourceType: 'module'
   }
 })
