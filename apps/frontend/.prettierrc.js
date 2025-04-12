ba4a2e0250caa0120a56b24fd1574c0011936020
Prabhu K H <25prabhu10@gmail.com>
2025-04-12 02:05:19 -0400

    feat: add tailwind css and shadcn ui components

--- apps/frontend/.prettierrc.js
+++ apps/frontend/.prettierrc.js
@@ -4,7 +4,9 @@ import pfPrettierConfig from '@pf/prettier-config'
  * @type {import("prettier").Config}
  */
 const config = {
-  ...pfPrettierConfig
+  ...pfPrettierConfig,
+  plugins: [...pfPrettierConfig.plugins, 'prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/index.css'
 }
 
 export default config
--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,5 +1,17 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button>Primary Button</Button>
+      <Button variant="secondary">Secondary Button</Button>
+      <Button variant="destructive">Destructive Button</Button>
+      <Button variant="outline">Outline Button</Button>
+      <Button variant="link">Link Button</Button>
+      <Button variant="ghost">Ghost Button</Button>
+    </>
+  )
 }
 
 export default App
